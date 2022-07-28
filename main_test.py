0e1bcb344f7fe34e21b3dab14d78a80613fd95a9
peerajak <peerajak@gmail.com>
2022-07-28 12:51:44 +0700

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,7 +1,8 @@
 import main;
 
 def test_hello():
-    set_keyboard_input(["2wf rwf e3rtg3 f 53tt3 er y"])
+    monkeypatch.setattr('sys.stdin', io.StringIO('2wf rwf e3rtg3 f 53tt3 er y'))
     main()
-    output = get_display_output()
+    captured = capsys.readouterr()
+    output = captured.out
     assert output == "2wf 53tt3 e3rtg3 er f rwf y"
