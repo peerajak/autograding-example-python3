adb7fe151e15119598ddd5e20a56b422f13d6c10
peerajak <peerajak@gmail.com>
2022-07-28 11:45:58 +0700

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,7 +1,7 @@
 import main;
 
 def test_hello():
-    set_keyboard_input([2wf rwf e3rtg3 f 53tt3 er y])
+    set_keyboard_input(["2wf rwf e3rtg3 f 53tt3 er y"])
     main()
     output = get_display_output()
     assert output == "2wf 53tt3 e3rtg3 er f rwf y"
